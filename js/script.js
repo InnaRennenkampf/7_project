f650108a7bec4af9c18785f7c1c05b02f5d7781e
inna_renn <inna.rennenkampf@gmail.com>
2023-02-15 13:59:12 +0200

    add dogs breeds

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-var url = "https://dog.ceo/api/breeds/image/random/3";
-
+var url = "https://dog.ceo/api/breeds/list/all";
 $.getJSON(url, function (data) {
     $.each(data.message, function (i, item) {
-        $('#data').append(
-            $('<img src=' + item + '>')
+        $('#dog-select').append(
+            $('<option value=' + i + '>' + i + '</option>')
         )
+    });
+});
+
+$('#dog-select').on('change', function () {
+    var url = "https://dog.ceo/api/breed/" + this.value + "/images";
+    $.getJSON(url, function (data) {
+        $('#data').empty();
+        $.each(data.message, function (i, item) {
+            $('#data').append(
+                $('<img src=' + item + '>')
+            )
+        });
     });
 });
