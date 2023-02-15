eae552251d6add109890bb860860693ef910b483
inna_renn <inna.rennenkampf@gmail.com>
2023-02-15 13:07:58 +0200

    multiple random dog images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-var url = "https://dog.ceo/api/breeds/image/random";
-$.getJSON(url, function (data) { // возвращает JSON
-    console.log(data);
-    $('#data').append(
-        $('<img src=' + data.message + '>')
-    )
+var url = "https://dog.ceo/api/breeds/image/random/3";
+
+$.getJSON(url, function (data) {
+    $.each(data.message, function (i, item) {
+        $('#data').append(
+            $('<img src=' + item + '>')
+        )
+    });
 });
